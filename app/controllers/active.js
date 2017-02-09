47ca7ded7bffe02c7be79d68e3150c009534f0f0
dzaki <dzaivan@gmail.com>
2017-02-09 13:51:36 +0100

    # Testing different syntax for computing properties (in route controller) - not using macros

--- app/controllers/active.js
+++ app/controllers/active.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	todos: Ember.computed.filterBy('model', 'completed', false)
+	todos: Ember.computed('model.@each.completed', function(){
+		return this.get('model').filterBy('completed', false);	
+	})
 });
